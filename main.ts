3f066afadb048d1933dd021892795d83d460701e
Malherbe Eric <114801688+MalherbeEric@users.noreply.github.com>
2024-01-18 11:33:02 +0100

    pb d'orthographe

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 /* Blocks pour utilisation de la carte i2c_moteur_grove
-    **PAS DE CODE POUR LE SPETTER **
+    **PAS DE CODE POUR LE STEPPER **
     ****** By Malherbe Eric - eric.malherbe@ac-orleans-tours.fr- ******/
 
 //% weight=100 color=#94280d icon="\uf1b9" block="I2c_Moteur_Grove"
